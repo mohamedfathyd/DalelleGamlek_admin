77184b02102a43e5ebd31ca4e6369fce102a56b6
mohamed fathy <m.fathy7793@gmail.com>
2019-06-04 22:25:35 +0200

    dasd

--- app/src/main/java/com/example/dalellegamlek_admin/model/apiinterface_home.java
+++ app/src/main/java/com/example/dalellegamlek_admin/model/apiinterface_home.java
@@ -11,41 +11,41 @@ import retrofit2.http.POST;
 
 public interface apiinterface_home {
     @FormUrlEncoded
-    @POST("Gmalek_add_category.php") Call<ResponseBody> getcontacts_add_first_category(@Field("name") String name, @Field("image") String image);
+    @POST("montag/dallejamalk/Gmalek_add_category.php") Call<ResponseBody> getcontacts_add_first_category(@Field("name") String name, @Field("image") String image);
     @FormUrlEncoded
-    @POST("Gmalek_add_annonce.php") Call<ResponseBody> getcontacts_add_annonce(@Field("image") String image);
+    @POST("montag/dallejamalk/Gmalek_add_annonce.php") Call<ResponseBody> getcontacts_add_annonce(@Field("image") String image);
 
     @FormUrlEncoded
-    @POST("Gmalek_delete_category.php")
+    @POST("montag/dallejamalk/Gmalek_delete_category.php")
     Call<ResponseBody> delete_first(@Field("id") int id);
 
-    @GET("Gmalek_annonce.php")
+    @GET("montag/dallejamalk/Gmalek_annonce.php")
     Call<List<contact_annonce>> getcontacts_annonce();
     @FormUrlEncoded
-    @POST("Gmalek_delete_annonce.php")
+    @POST("montag/dallejamalk/Gmalek_delete_annonce.php")
     Call<ResponseBody> delete_annonce(@Field("id") int id);
-    @GET("Gmalek_first_category.php")
+    @GET("montag/dallejamalk/Gmalek_first_category.php")
     Call<List<contact_category>> getcontacts_allfirst();
-    @GET("Gmalek_all_users.php")
+    @GET("montag/dallejamalk/Gmalek_all_users.php")
     Call<List<contact_users>> getcontacts_users();
-    @GET("Gmalek_all_providers_notaccepted.php")
+    @GET("montag/dallejamalk/Gmalek_all_providers_notaccepted.php")
     Call<List<contact_users>> getcontacts_provider_not();
 
     @FormUrlEncoded
-    @POST("Gmalek_accept_provider.php")
+    @POST("montag/dallejamalk/Gmalek_accept_provider.php")
     Call<ResponseBody> accept_provider(@Field("id") int id);
-    @GET("Gmalek_all_providers_accepted.php")
+    @GET("montag/dallejamalk/Gmalek_all_providers_accepted.php")
     Call<List<contact_users>> getcontacts_provider_acc();
     @FormUrlEncoded
-    @POST("Gmalek_delete_provider.php")
+    @POST("montag/dallejamalk/Gmalek_delete_provider.php")
     Call<ResponseBody> accept_provider_delete(@Field("id") int id);
-    //Gmalek_update_point_provider.php
+    //montag/dallejamalk/Gmalek_update_point_provider.php
     @FormUrlEncoded
-    @POST("Gmalek_update_point_provider.php")
+    @POST("montag/dallejamalk/Gmalek_update_point_provider.php")
     Call<ResponseBody> charge_provider(@Field("id") int id,@Field("points")int points);
-    @GET("Gmalek_all_charge_event.php")
+    @GET("montag/dallejamalk/Gmalek_all_charge_event.php")
     Call<List<content_event>> getcontacts_charge();
-    @GET("Gmalek_all_pull_event.php")
+    @GET("montag/dallejamalk/Gmalek_all_pull_event.php")
     Call<List<content_event>> getcontacts_pull();
 }
 
